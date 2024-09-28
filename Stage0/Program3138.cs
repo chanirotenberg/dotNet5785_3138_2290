dc32b8a6b4e4d04f7eafc04a89ebaaacdfe874fc
רוטנברג חנה <Hand4d29@mbjcomp.org.il>
2024-09-28 22:44:24 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program3138.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome3138();
+        Welcome2290();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome2290();
+
+    private static void Welcome3138()
+    {
+        Console.WriteLine("Enter your name:");
+        string x = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console ", x);
+    }
+}
