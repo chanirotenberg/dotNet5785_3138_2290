abacfd06f5baefbc4a7c09507db96bc2fa2a07a3
רוטנברג חנה <Hand4d29@mbjcomp.org.il>
2024-09-28 23:26:27 +0300

    NewWelcome

--- Stage0/Program3138.cs
+++ Stage0/Program3138.cs
@@ -1,18 +1,23 @@
-﻿partial class Program
+﻿using System;
+
+namespace Stage0
 {
-    private static void Main(string[] args)
+    partial class Program
     {
-        Welcome3138();
-        Welcome2290();
-        Console.ReadKey();
-    }
+        private static void Main(string[] args)
+        {
+            Welcome3138();
+            Welcome2290();
+            Console.ReadKey();
+        }
 
-    static partial void Welcome2290();
+        static partial void Welcome2290();
 
-    private static void Welcome3138()
-    {
-        Console.WriteLine("Enter your name:");
-        string x = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console ", x);
+        private static void Welcome3138()
+        {
+            Console.WriteLine("Enter your name:");
+            string x = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console ", x);
+        }
     }
 }
