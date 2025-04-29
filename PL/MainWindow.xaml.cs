da347709575defc2ceb946d8637be128c88a75f6
רוטנברג חנה <Hand4d29@mbjcomp.org.il>
2025-04-29 13:41:41 +0300

    up to chapter 7 included

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PL.Volunteer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,129 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Minute);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Hour);
+        }
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Day);
+        }
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Month);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.Year);
+        }
+
+        public TimeSpan RiskRange
+        {
+            get { return (TimeSpan)GetValue(RiskRangeProperty); }
+            set { SetValue(RiskRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty RiskRangeProperty =
+            DependencyProperty.Register("RiskRange", typeof(TimeSpan), typeof(MainWindow));
+        private void UpdateRiskRange_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.SetRiskRange(RiskRange);
+        }
+        private void clockObserver()
+        {
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+        private void configObserver()
+        {
+            RiskRange = s_bl.Admin.GetRiskRange();
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // השמה של השעון
+            CurrentTime = s_bl.Admin.GetClock();
+
+            // השמה של משתני התצורה
+            RiskRange = s_bl.Admin.GetRiskRange();
+            // אם יש עוד משתני תצורה - תעשה גם להם השמה כאן
+
+            // רישום השקיפים
+            s_bl.Admin.AddClockObserver(clockObserver);
+            s_bl.Admin.AddConfigObserver(configObserver);
+        }
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            // הסרת ההשקפה על השעון
+            s_bl.Admin.RemoveClockObserver(clockObserver);
+
+            // הסרת ההשקפה על משתני התצורה
+            s_bl.Admin.RemoveConfigObserver(configObserver);
+        }
+        private void InitializeDB_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("האם אתה בטוח שברצונך לאתחל את בסיס הנתונים?", "אישור אתחול", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Mouse.OverrideCursor = Cursors.Wait; // שינוי סמן לשעון חול
+                try
+                {
+                    // סגירת כל המסכים הפתוחים חוץ מהמסך הראשי
+                    foreach (Window window in Application.Current.Windows)
+                    {
+                        if (window != this)
+                            window.Close();
+                    }
+
+                    s_bl.Admin.InitializeDB(); // קריאה לאתחול
+                }
+                finally
+                {
+                    Mouse.OverrideCursor = null; // החזרת סמן רגיל
+                }
+            }
+        }
+
+        private void ResetDB_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("האם אתה בטוח שברצונך לאפס את בסיס הנתונים?", "אישור איפוס", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Mouse.OverrideCursor = Cursors.Wait; // שינוי סמן לשעון חול
+                try
+                {
+                    // סגירת כל המסכים הפתוחים חוץ מהמסך הראשי
+                    foreach (Window window in Application.Current.Windows)
+                    {
+                        if (window != this)
+                            window.Close();
+                    }
+
+                    s_bl.Admin.ResetDB(); // קריאה לאיפוס
+                }
+                finally
+                {
+                    Mouse.OverrideCursor = null; // החזרת סמן רגיל
+                }
+            }
+        }
+        private void HandleVolunteer_Click(object sender, RoutedEventArgs e)
+        {
+            new VolunteerListWindow().Show(); // פתיחת מסך רשימת הקורסים בלי לחסום את המסך הראשי
         }
     }
 }
--- PL/Volunteer/VolunteerListWindow.xaml.cs
+++ PL/Volunteer/VolunteerListWindow.xaml.cs
@@ -14,14 +14,27 @@ using System.Windows.Shapes;
 
 namespace PL.Volunteer
 {
+
     /// <summary>
     /// Interaction logic for VolunteerListWindow.xaml
     /// </summary>
     public partial class VolunteerListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
         public VolunteerListWindow()
         {
             InitializeComponent();
         }
+        public IEnumerable<BO.VolunteerInList> CourseList
+        {
+            get { return (IEnumerable<BO.VolunteerInList>)GetValue(VolunteerListProperty); }
+            set { SetValue(VolunteerListProperty, value); }
+        }
+
+        public static readonly DependencyProperty VolunteerListProperty =
+            DependencyProperty.Register("VolunteerList", typeof(IEnumerable<BO.VolunteerInList>), typeof(VolunteerListWindow), new PropertyMetadata(null));
+
     }
 }
+
